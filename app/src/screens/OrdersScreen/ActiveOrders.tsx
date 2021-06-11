5cf55485eda5c81a3cccc798d1296f309c6a95f6
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2021-06-11 10:38:05 +0200

    Create tabNavigator in OrdersScreen

--- app/src/screens/OrdersScreen/ActiveOrders.tsx
+++ app/src/screens/OrdersScreen/ActiveOrders.tsx
@@ -1,24 +1,22 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import {useNavigation} from '@react-navigation/core';
 import React, {useEffect, useContext} from 'react';
-import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import {StyleSheet, Text, View, FlatList, TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
+import {OrderCard} from '../../components/OrderCard';
 import {globalStyles} from '../../theme/appTheme';
 import {Colors} from '../../theme/colors';
 import {CartContext} from '../../store/context/CartContext';
 import {AuthContext} from '../../store/context/AuthContext';
 import {getAllOrders} from '../../services/orders';
 import {OrdersContext} from '../../store/context/OrdersContext';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {NavigationContainer} from '@react-navigation/native';
-import {ActiveOrders} from './ActiveOrders';
 
 interface RouteParams {
   restaurantId: string;
   tableId: string;
 }
 
-export const OrdersScreen = () => {
+export const ActiveOrders = () => {
   const navigation: any = useNavigation();
   const {
     cartState: {cart},
@@ -67,64 +65,29 @@ export const OrdersScreen = () => {
     });
   }, [navigation, cart]);
 
-  const Tab = createBottomTabNavigator();
-
-  const FinishedOrders = () => (
-    <View style={styles.totalContainer}>
-      <Text style={styles.totalText}>Finished orders</Text>
-    </View>
-  );
+  const renderItem = ({item}: any) => <OrderCard order={item} />;
 
   return ordersState.orders ? (
-    <NavigationContainer independent={true}>
-      <Tab.Navigator
-        screenOptions={({route}) => ({
-          tabBarIcon: ({color, size}) => {
-            let iconName;
-
-            if (route.name === 'Active') {
-              iconName = 'hourglass-outline';
-            } else if (route.name === 'Finished') {
-              iconName = 'checkbox-outline';
-            }
-
-            // You can return any component that you like here!
-            return (
-              <Icon
-                name={iconName ? iconName : 'ellipsis-horizontal'}
-                size={size}
-                color={color}
-              />
-            );
-          },
-        })}
-        tabBarOptions={{
-          activeTintColor: Colors.primary,
-          inactiveTintColor: Colors.dark,
-        }}>
-        <Tab.Screen name="Active" component={ActiveOrders} />
-        <Tab.Screen name="Finished" component={FinishedOrders} />
-        <Tab.Screen
-          name="Button"
-          component={ActiveOrders}
-          options={{
-            tabBarButton: () => (
-              <TouchableOpacity
-                style={styles.btnTabContainer}
-                onPress={() =>
-                  navigation.navigate('StackNavigator', {
-                    screen: 'RestaurantScreen',
-                  })
-                }>
-                <View style={styles.btnTab}>
-                  <Text style={styles.btnTabText}>New Order</Text>
-                </View>
-              </TouchableOpacity>
-            ),
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <View style={globalStyles.frameContainer}>
+      <View style={globalStyles.frame}>
+        <View style={styles.container}>
+          <Text style={globalStyles.title}>Active orders</Text>
+          <FlatList
+            data={ordersState.orders}
+            renderItem={renderItem}
+            keyExtractor={item => item._id}
+          />
+          <View style={styles.totalContainer}>
+            <Text style={styles.totalText}>total:</Text>
+          </View>
+          <View style={styles.btnContainer}>
+            <TouchableOpacity style={[globalStyles.bigButton, styles.btnLogin]}>
+              <Text style={globalStyles.bigButtonText}>Confirm</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </View>
   ) : (
     <Text>No products</Text>
   );
@@ -182,24 +145,5 @@ const styles = StyleSheet.create({
   totalText: {
     fontSize: 26,
     color: Colors.dark,
-  },
-  btnTabContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 5,
-  },
-  btnTab: {
-    flex: 1,
-    width: '80%',
-    padding: 2,
-    marginTop: 3,
-    borderRadius: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: Colors.primary,
-  },
-  btnTabText: {
-    color: Colors.white,
   },
 });
